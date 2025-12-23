db10c1ce9da0a8d22687718a918b517d5ddafeac
Ivasik <pouncevokno8@gmail.com>
2025-12-23 12:57:08 +0400

    setup: настройка allias для папок в проекте

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url';
+
 export default defineNuxtConfig({
   modules: ['@nuxt/test-utils', '@nuxt/eslint'],
   devtools: { enabled: true },
@@ -16,6 +18,21 @@ export default defineNuxtConfig({
       spotifyAccountsUrl: '',
       spotifyBaseUrl: '',
     },
+  },
+  alias: {
+    '#content': fileURLToPath(new URL('./content', import.meta.url)),
+    '#modules': fileURLToPath(new URL('./modules', import.meta.url)),
+    '#public': fileURLToPath(new URL('./public', import.meta.url)),
+    '#server': fileURLToPath(new URL('./server', import.meta.url)),
+    '#shared': fileURLToPath(new URL('./shared', import.meta.url)),
+    '#assets': fileURLToPath(new URL('./app/assets', import.meta.url)),
+    '#components': fileURLToPath(new URL('./app/components', import.meta.url)),
+    '#composables': fileURLToPath(new URL('./app/composables', import.meta.url)),
+    '#layouts': fileURLToPath(new URL('./app/layouts', import.meta.url)),
+    '#middleware': fileURLToPath(new URL('./app/middleware', import.meta.url)),
+    '#pages': fileURLToPath(new URL('./app/pages', import.meta.url)),
+    '#plugins': fileURLToPath(new URL('./app/plugins', import.meta.url)),
+    '#utils': fileURLToPath(new URL('./app/utils', import.meta.url)),
   },
   compatibilityDate: '2025-07-15',
   eslint: {
