0c26c9ed6578cc6c74b6e001a599587ca85d07d4
Ivasik <pouncevokno8@gmail.com>
2025-12-23 10:02:05 +0400

    setup: настройка переменных окружения в nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,22 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/test-utils', '@nuxt/eslint'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Nuxt Player',
+    },
+  },
+  runtimeConfig: {
+    clientId: '',
+    codeVerifier: '',
+
+    public: {
+      env: '',
+      redirectUrl: '',
+      spotifyAccountsUrl: '',
+      spotifyBaseUrl: '',
+    },
+  },
   compatibilityDate: '2025-07-15',
   eslint: {
     config: {
