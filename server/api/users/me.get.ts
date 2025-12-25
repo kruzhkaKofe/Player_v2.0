33668b3d0193bbba597a410493faf8380821d134
Ivasik <pouncevokno8@gmail.com>
2025-12-25 20:49:14 +0400

    refactor: метод обработки ошибок перенесен в #shared/utils

--- server/api/users/me.get.ts
+++ server/api/users/me.get.ts
@@ -1,6 +1,6 @@
 import { meSchema } from '#shared/types/users.types';
 import { createSpotifyApi } from '#server/utils/createSpotifyApi';
-import { errorHandler } from '#server/utils/errorHandler';
+import { errorHandler } from '#shared/utils/errorHandler';
 
 export default defineEventHandler(event => {
   const api = createSpotifyApi(event);
--- shared/utils/errorHandler.ts
+++ shared/utils/errorHandler.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-
+import { createError } from 'h3';
 import {
   errorsTypes,
   SpotifyRegularErrorSchema,
